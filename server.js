d908c1c8d46f56a7136d003f51fbbaca08438b9a
Danika Pearson <89046934+karmadog72@users.noreply.github.com>
2021-11-19 16:24:56 -0700

    addded htmls & css links

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const express = require("express");
-const { animals } = require("./data/animals");
 const fs = require("fs");
 const path = require("path");
-
+const express = require("express");
+const { animals } = require("./data/animals.json");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.static("zookeepr-public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -51,8 +54,6 @@ function createNewAnimal(body, animalsArray) {
     path.join(__dirname, "./data/animals.json"),
     JSON.stringify({ animals: animalsArray }, null, 2)
   );
-
-  // return finished code to post route for response
   return animal;
 }
 
@@ -89,22 +90,45 @@ app.get("/api/animals/:id", (req, res) => {
   }
 });
 
-// parse incoming string or array data
-app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
-app.use(express.json());
+// // parse incoming string or array data
+// app.use(express.urlencoded({ extended: true }));
+// // parse incoming JSON data
+// app.use(express.json());
 
 app.post("/api/animals", (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
 
-  // if any data in req.body is incorrect, send 400 error back
   if (!validateAnimal(req.body)) {
     res.status(400).send("The animal is not properly formatted.");
   } else {
     const animal = createNewAnimal(req.body, animals);
     res.json(animal);
   }
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/zookeepr-public/index.html"));
+});
+
+app.get("/animals", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/zookeepr-public/animals.html"));
+});
+
+app.get("/zookeepers", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "./public/zookeepr-public/zookeepers.html")
+  );
+});
+
+// app.get("/style.css", (req, res) => {
+//   res.sendFile(
+//     path.join(__dirname, "./public/zookeepr-public/assets/css/style.css")
+//   );
+// });
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/zookeepr-public/index.html"));
 });
 
 app.listen(PORT, () => {
